baeb48d9cef8560f3228dafddf21714ee4015bf6
VAPY05 <74071882+VAPY05@users.noreply.github.com>
2022-07-01 19:54:39 +0300

    Create React App and adding JSX Menu Component

---
+++ stereotype-streaming-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Menu} from '../src/Components/Menu/Menu'
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
---
+++ stereotype-streaming-app/src/Components/Menu/Menu.js
@@ -0,0 +1,27 @@
+import './Menu.css';
+export const Menu = () => {
+    return (
+        <div>
+            <div className="navbar">
+            <div className="navbar-container">
+                <div className="logo-container"><h1 className="logo">Stereotype Streaming</h1></div>
+                <div className="menu-container">
+                    <ul className="menu-list">
+                        <li className="menu-list item">Home</li>
+                        <li className="menu-list item">Movies</li>
+                        <li className="menu-list item">Series</li>
+                        <li className="menu-list item">Popular</li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+    <div className="sidebar">
+        <i className="left-menu-icon fa-solid fa-magnifying-glass"></i>
+        <i className="left-menu-icon fa-solid fa-tv"></i>
+        <i className="left-menu-icon fa-solid fa-home"></i>
+        <i className="left-menu-icon fa-solid fa-user"></i>
+        <i className="left-menu-icon fa-solid fa-bookmark"></i>
+    </div>
+    </div>
+    )
+}
