974c3693e2368469dbd0afd40f5700d915e9e2cd
Valentin Kotov <74071882+VAPY05@users.noreply.github.com>
2022-07-14 04:57:37 -0700

    adding profile page

--- client/stereotype-streaming-app/src/App.js
+++ client/stereotype-streaming-app/src/App.js
@@ -9,20 +9,10 @@ import {MovieList} from '../src/Components/Movies/Movies'
 import { NotFound } from './Components/404/404';
 import { VideoPlayer } from './Components/Video/VideoPlayer';
 import { Details } from './Components/Details/Details';
+import { Profile } from './Components/Profile/Profile';
+import {Register} from "./Components/Profile/Register/Register"
 
 function App() {
-  /* return (
-    <div>
-      <Menu>
-        <div classNameName='main'>
-        <MovieList/>
-        </div>
-      </Menu>
-    </div> 
-  ); */
-
-  
-
   return(
     <div>
     <BrowserRouter>
@@ -32,6 +22,8 @@ function App() {
         <Route path='/search' element={<Search/>}></Route>
         <Route path='/movies/:id/details' element={<Details/>}></Route>
         <Route path='/movies/:id/watch' element={<VideoPlayer/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+        <Route path='/profile/auth' element={<Register/>}></Route>
         <Route path='*' element={<NotFound/>}></Route>
       </Routes>
     </BrowserRouter>
