28d8f554b295a7cdce4c7de36a4cfb06f3f70118
Valentin Kotov <74071882+VAPY05@users.noreply.github.com>
2022-07-16 07:29:16 -0700

    adding login and register forms

--- client/stereotype-streaming-app/src/Components/Profile/Login/Login.js
+++ client/stereotype-streaming-app/src/Components/Profile/Login/Login.js
@@ -1,19 +1,37 @@
-import {useState} from "react"
+import { useState } from "react"
 
 import "./Login.css"
 
+import {Link} from "react-router-dom" 
+
 export const Login = () => {
 
-	const [username, setUsername] = useState('Username')
-	const [password, setPassword] = useState('Password')
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
 
 	function session() {
 
 	}
 
-    return (
-	<>	
-		<h1>Login</h1>
-	</>
-    )
+	return (
+		<>
+			<div className="login-page">
+				<div className="form">
+					{/* <form className="register-form">
+						<input type="text" placeholder="name" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+						<input type="password" placeholder="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+						<input type="text" placeholder="email address" />
+						<button>create</button>
+						<p className="message">Already registered? <a href="#">Sign In</a></p>
+					</form> */}
+					<form className="login-form">
+						<input type="text" placeholder="name" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+						<input type="password" placeholder="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+						<button>login</button>
+						<p className="message">Not registered? <Link to={"/profile/register"}>Create an account</Link></p>
+					</form>
+				</div>
+			</div>
+		</>
+	)
 }
--- client/stereotype-streaming-app/src/Components/Profile/Register/Register.js
+++ client/stereotype-streaming-app/src/Components/Profile/Register/Register.js
@@ -2,10 +2,14 @@ import {useState} from "react"
 
 import "./Register.css"
 
+import {Link} from "react-router-dom" 
+
+
 export const Register = () => {
 
-	const [username, setUsername] = useState('Username')
-	const [password, setPassword] = useState('Password')
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	const [repeatedPassword, setRepeatedPassword] = useState('')
 
 	function session() {
 
@@ -13,7 +17,17 @@ export const Register = () => {
 
     return (
 	<>	
-		<h1>Register</h1>
+		<div className="login-page">
+				<div className="form">
+					<form className="login-form">
+						<input type="text" placeholder="name" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+						<input type="password" placeholder="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+						<input type="password" placeholder="repeat password" value={repeatedPassword} onChange={(e)=>{setRepeatedPassword(e.target.value)}}/>
+						<button>Register</button>
+						<p className="message">Already registered? <Link to={"/profile/login"}>Sing in</Link></p>
+					</form>
+				</div>
+			</div>
 	</>
     )
 }
