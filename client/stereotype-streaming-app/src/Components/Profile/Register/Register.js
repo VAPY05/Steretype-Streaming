f4ecfe7e449ee11b07b62789e8322d27d37fb788
Valentin Kotov <74071882+VAPY05@users.noreply.github.com>
2022-07-16 07:49:34 -0700

    Update Register.js

--- client/stereotype-streaming-app/src/Components/Profile/Register/Register.js
+++ client/stereotype-streaming-app/src/Components/Profile/Register/Register.js
@@ -20,7 +20,7 @@ export const Register = () => {
 		<div className="login-page">
 				<div className="form">
 					<form className="login-form">
-						<input type="text" placeholder="name" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+						<input type="text" placeholder="username" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
 						<input type="password" placeholder="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
 						<input type="password" placeholder="repeat password" value={repeatedPassword} onChange={(e)=>{setRepeatedPassword(e.target.value)}}/>
 						<button>Register</button>
