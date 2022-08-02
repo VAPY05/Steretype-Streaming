378e74f2db817a32b6da2606dc8491152f9a6d75
Valentin Kotov <74071882+VAPY05@users.noreply.github.com>
2022-08-01 23:27:24 -0700

    fixes

--- client/stereotype-streaming-app/src/App.js
+++ client/stereotype-streaming-app/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import {Menu} from '../src/Components/Menu/Menu'
-import {Search} from '../src/Components/Search/Search'
-
-import {MovieList} from '../src/Components/Movies/Movies'
+import { Menu } from '../src/Components/Menu/Menu'
+import { Search } from '../src/Components/Search/Search'
+import { MovieList } from '../src/Components/Movies/Movies'
 import { NotFound } from './Components/404/404';
 import { VideoPlayer } from './Components/Video/VideoPlayer';
 import { Details } from './Components/Details/Details';
@@ -14,24 +13,39 @@ import { Login } from './Components/Profile/Login/Login';
 import { Register } from './Components/Profile/Register/Register';
 import { CreateMovie } from './Components/CreateMovie/createMovie';
 import { EditMovie } from './Components/EditMovie/editMovie';
+import { MoviesAll } from './Components/Movies/MoviesAll/MoviesAll';
+
+import { authContext } from './contexts/authContext';
+import { useState } from 'react';
 
 function App() {
-  return(
-    <div>
-      <Menu/>
-      <Routes>
-        <Route path='/' element={<MovieList/>}></Route>
-        <Route path='/search' element={<Search/>}></Route>
-        <Route path='/movies/:id/details' element={<Details/>}></Route>
-        <Route path='/movies/:id/watch' element={<VideoPlayer/>}></Route>
-        <Route path='/profile' element={<Profile/>}></Route>
-        <Route path='/profile/login' element={<Login/>}></Route>
-        <Route path='/profile/register' element={<Register/>}></Route>
-        <Route path='/movies/add-movie' element={<CreateMovie/>}></Route>
-        <Route path='/movies/:id/edit' element={<EditMovie/>}></Route>
-        <Route path='*' element={<NotFound/>}></Route>
-      </Routes>
-    </div>
+
+  const [auth, setAuth] = useState({});
+
+  const loginHandler = (authData) => {
+    setAuth(authData)
+  }
+
+  return (
+    <authContext.Provider value={{ user:auth, loginHandler }}>
+      <div>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<MovieList />}></Route>
+          <Route path='/search' element={<Search />}></Route>
+          <Route path='/movies/:id/details' element={<Details />}></Route>
+          <Route path='/movies/:id/watch' element={<VideoPlayer />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+          <Route path='/profile/login' element={<Login />}></Route>
+          <Route path='/profile/register' element={<Register />}></Route>
+          <Route path='/movies/add-movie' element={<CreateMovie />}></Route>
+          <Route path='/movies/:id/edit' element={<EditMovie />}></Route>
+          <Route path='/movies/new' element={<MoviesAll type={"new"} />}></Route>
+          <Route path='/movies' element={<MoviesAll type={""} />}></Route>
+          <Route path='*' element={<NotFound />}></Route>
+        </Routes>
+      </div>
+    </authContext.Provider>
   )
 }
 
