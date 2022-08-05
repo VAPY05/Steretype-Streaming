fb2644fc3bfefeb760ba3423bd34c31609b90f89
Valentin Kotov <74071882+VAPY05@users.noreply.github.com>
2022-08-05 12:50:04 +0300

    updating components folder

--- client/src/Components/404/404.js
+++
@@ -1,7 +0,0 @@
-import "./404.css"
-
-export const NotFound = () => {
-    return (
-        <h1 className="notFound">404 Not Found</h1>
-    )
-}
--- client/src/Components/Loading/Loading.js
+++
@@ -1,8 +0,0 @@
-import loading from './256x256.gif'
-import "./Loading.css"
-
-export const Loading = () => {
-    return (
-        <img src={loading} className="Loading"/>
-    )
-} 
